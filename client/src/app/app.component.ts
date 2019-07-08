d4f0fe0f7a6df9640a27b2bb4c654992f8c88972
Guille Puntons <guillermo.puntons@vml.com>
2019-07-08 17:14:38 -0300

    hasta clase 62 - cerrar sesion (incluida)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {UserService} from './services/user.service';
 import { User } from './models/user';
+import { identity } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [UserService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'Musify';
   public user: User;
-  public identity = false;
+  public identity;
   public token;
+  public errorMessage;
 
-  constructor(){
-    this.user = new User('','','','','','ROLE_USER','');
+  constructor(private _userService:UserService){
+    this.user = new User('','','','','','ROLE_USER','');    
   }
 
+  ngOnInit(){
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    console.log('in ngOnInit');
+    console.log(this.identity);
+    console.log(this.token);
+  }
+
+
   onSubmit(){
-    console.log(this.user)
+    console.log(this.user);
+
+    // Conseguir los datos del user identificado
+    this._userService.singup(this.user).subscribe(
+      response =>{
+        console.log(response);
+        let identity = response.user;
+        this.identity = identity;
+
+        if(!this.identity._id){
+          alert("El usuario no esta correctamente identificado");          
+        }else{
+          // Crear elemento en el local starage para tener al usuario en sesion
+          localStorage.setItem('identity',JSON.stringify(identity));
+
+          // Conseguir el token para enviarlo a cada peticion
+          this._userService.singup(this.user,'true').subscribe(
+            response =>{
+              console.log(response);
+              let token = response.token;
+              this.token = token;
+      
+              if(this.token.length <= 0){
+                alert("El token es incorrecto");          
+              }else{
+                // Crear elemento en el local starage para tener el token disponible
+                localStorage.setItem('token',token);
+                console.log(token);
+                console.log(identity);
+                
+              }
+      
+            },
+            error => {
+              var errorMessage = <any>error;
+              if(errorMessage != null){
+                var body = JSON.parse(error._body);
+                this.errorMessage = body.message;
+                console.log(error);
+              }
+            }
+          );
+        }
+
+      },
+      error => {
+        var errorMessage = <any>error;
+        if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.errorMessage = body.message;
+          console.log(error);
+        }
+      }
+    );
+  }
+
+  
+  logout(){
+    //puedo usar este metodo y remover todo
+    localStorage.clear();
+    //o remover item por item usando la clave
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    this.identity = null;
+    this.token = null;
   }
 
 }
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,53 @@
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import {GLOBAL} from './global';
+
+@Injectable()
+export class UserService{
+    public identity;
+    public token;
+
+    public url: string;
+
+    constructor(public _http: Http){
+        this.url = GLOBAL.url
+    }
+
+    singup(user_to_login, gethash = null){
+        if(gethash != null){
+            user_to_login.gethash = gethash;
+        }
+        let json = JSON.stringify(user_to_login);
+        let params = json;
+
+        let headers = new Headers({'Content-type':'application/json'});
+
+        return this._http.post(this.url+'login',params,{headers:headers}).map(res => res.json());
+    }
+
+    getIdentity(){
+        let identity = JSON.parse(localStorage.getItem('identity'));
+        
+        if(identity != "undefined"){
+            this.identity = identity;
+        }else{
+            this.identity = null;
+        }
+        return this.identity;
+    }
+
+    getToken(){
+        let token = localStorage.getItem('token');
+        
+        if(token != "undefined"){
+            this.token = token;
+        }else{
+            this.token = null;
+        }
+        return this.token;
+
+    }
+
+}
